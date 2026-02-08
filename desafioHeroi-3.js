af02e8d90aafbd1b2b7a35ca2b95d6e2d438083f
Gab-la <gabrielcorreaglbc@gmail.com>
2026-02-08 12:41:18 -0300

    fim do desafio

--- desafioHeroi-3.js
+++ desafioHeroi-3.js
@@ -1,5 +1,24 @@
-class classeHeroi{
-    constructor(){
+class infoHero{
+    constructor(nome,classe, nivel){
+    this.nome=nome
+     this.classe=classe
+    this.nivel=nivel
+   
+}escrever(){
+    console.log(`ola ${this.nome},seu ${this.classe} está no nivel: ${this.nivel}.`)
+}
+}
 
+class ataqueHeroi{
+    constructor(heroi,ataque,){
+        this.heroi=heroi
+        this.ataque=ataque
+
+    }escrever (){
+        console.log(`o tipo ${this.heroi.classe} atacou usando ${this.ataque}.`)
     }
 }
+let info = new infoHero ("Slimmshady","Ninja", 651)
+let ataque = new ataqueHeroi (info, "shuriken")
+info.escrever()
+ataque.escrever()
